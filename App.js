a59f854454914792ab2d0a65440a71cefbf748b8
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-10 15:10:49 +0530

    yellow warning removed

--- App.js
+++ App.js
@@ -5,6 +5,16 @@ import CreateAccount from './components/CreateAccount';
 import { createAppContainer } from "react-navigation";
 
 import { createStackNavigator } from "react-navigation-stack";
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
 
 const RootStack = createStackNavigator({
   Login: { screen: Login },
